3877b43f6f8db48531a85904cfea279452a71373
allec <alleckey96@gmail.com>
2023-06-06 00:58:39 +0200

    Day-59_blog_bootstrap_project

--- day-_59_blog_project/server.py
+++ day-_59_blog_project/server.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
+import requests
+
+posts_url = requests.get("https://api.npoint.io/f5b77c12eccc9832cbcf")
+posts = posts_url.json()
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
-    return render_template("index.html")
-
-
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
 @app.route("/about")
 def about():
@@ -16,6 +17,15 @@ def about():
 @app.route('/contact')
 def contact():
     return  render_template("contact.html")
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post= None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post=blog_post
+    return render_template("post.html", post=requested_post)
+
 
 
 if __name__ == "__main__":
