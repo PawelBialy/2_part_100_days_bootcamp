0f48a22f604525d9dd78a6029110106aa0d521d3
PawelBialy <pbialy96@gmail.com>
2023-06-13 22:04:21 +0200

    day_61_Flask_WTForms

---
+++ day_61_Flask_WTForms/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+app = Flask(__name__)
+
+app.secret_key = "somesecretstring"
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label="Log In")
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+    return render_template("login.html", form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
