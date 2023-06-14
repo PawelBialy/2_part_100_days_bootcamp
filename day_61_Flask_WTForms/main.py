46e8f2ba0b04e8d3b08d8e37ce1db2becef49328
PawelBialy <pbialy96@gmail.com>
2023-06-14 23:44:41 +0200

    day_61_Flask_WTForms

--- day_61_Flask_WTForms/main.py
+++ day_61_Flask_WTForms/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, InputRequired, Length
 
 app = Flask(__name__)
 
@@ -8,18 +9,21 @@ app.secret_key = "somesecretstring"
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
-    submit = SubmitField(label="Log In")
+    email = StringField(label='Email', validators=[DataRequired(), Email(check_deliverability=True)])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, message=None)])
+    submit = SubmitField(label='Login')
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+
     return render_template("login.html", form=login_form)
 
 
