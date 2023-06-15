a561b5930a6516b8c92542cc1e4ff31c6a70af2e
PawelBialy <pbialy96@gmail.com>
2023-06-15 13:09:34 +0200

    day_61_Flask_WTForms

--- day_61_Flask_WTForms/main.py
+++ day_61_Flask_WTForms/main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, InputRequired, Length
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-
+Bootstrap(app)
 app.secret_key = "somesecretstring"
-
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email(check_deliverability=True)])
@@ -18,12 +20,18 @@ class LoginForm(FlaskForm):
 def home():
     return render_template('index.html')
 
+@app.route("/button", methods=["POST"])
+def button():
+    return redirect(url_for('login'))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "123456789" :
+            return  render_template("success.html")
+        else :
+            return render_template('denied.html')
     return render_template("login.html", form=login_form)
 
 
